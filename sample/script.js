444e6ac771551901fd846fbf086375302eda6cb0
z.esmaeilpour <z.esmaeilpour49@gmail.com>
2023-10-15 02:11:59 +0330

    initial commit

---
+++ sample/script.js
@@ -0,0 +1,35 @@
+const watchdog = new CKSource.EditorWatchdog();
+
+window.watchdog = watchdog;
+
+watchdog.setCreator( ( element, config ) => {
+	return CKSource.Editor
+		.create( element, config )
+		.then( editor => {
+			return editor;
+		} );
+} );
+
+watchdog.setDestructor( editor => {
+	return editor.destroy();
+} );
+
+watchdog.on( 'error', handleSampleError );
+
+watchdog
+	.create( document.querySelector( '.editor' ), {
+		// Editor configuration.
+	} )
+	.catch( handleSampleError );
+
+function handleSampleError( error ) {
+	const issueUrl = 'https://github.com/ckeditor/ckeditor5/issues';
+
+	const message = [
+		'Oops, something went wrong!',
+		`Please, report the following error on ${ issueUrl } with the build id "c7cxrno5c40m-7b1o629jleoy" and the error stack trace:`
+	].join( '\n' );
+
+	console.error( message );
+	console.error( error );
+}
